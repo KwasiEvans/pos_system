056ab984c91e467e7343cded12d6393ca517cc6f
KwasiEvans <evansow2002@gmail.com>
2023-07-17 10:24:32 -1200

    completed functionality of registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,30 +15,39 @@
 <h3>Create an Account</h3>
 <h4>Continue where you left off</h4>
 </div>
-<div class="form-login">
-<label>Full Name</label>
-<div class="form-addons">
-<input type="text" placeholder="Enter your full name">
-<img src="assets/img/icons/users1.svg" alt="img">
-</div>
-</div>
-<div class="form-login">
-<label>Email</label>
-<div class="form-addons">
-<input type="text" placeholder="Enter your email address">
-<img src="assets/img/icons/mail.svg" alt="img">
-</div>
-</div>
-<div class="form-login">
-<label>Password</label>
-<div class="pass-group">
-<input type="password" class="pass-input" placeholder="Enter your password">
-<span class="fas toggle-password fa-eye-slash"></span>
-</div>
-</div>
-<div class="form-login">
-<a class="btn btn-login">Sign Up</a>
-</div>
+ <form method="POST" action="{{ route('register') }}">@csrf
+    <div class="form-login">
+        <label>Full Name</label>
+        <div class="form-addons">
+        <input type="text" placeholder="Enter your full name" name="name">
+        <img src="assets/img/icons/users1.svg" alt="img">
+        </div>
+        </div>
+        <div class="form-login">
+        <label>Email</label>
+        <div class="form-addons">
+        <input type="text" placeholder="Enter your email address" name="email">
+        <img src="assets/img/icons/mail.svg" alt="img">
+        </div>
+        </div>
+        <div class="form-login">
+        <label>Password</label>
+        <div class="pass-group">
+        <input type="password" class="pass-input" placeholder="Enter your password" name="password">
+        <span class="fas toggle-password fa-eye-slash"></span>
+        </div>
+        </div>
+        <div class="form-login">
+        <label>Confirm Password</label>
+        <div class="pass-group">
+        <input type="password" class="pass-input" placeholder="Enter your password" name="password_confirmation">
+        <span class="fas toggle-password fa-eye-slash"></span>
+        </div>
+        </div>
+        <div class="form-login">
+        <button type="submit" class="btn btn-login">Sign Up</button>
+        </div>
+ </form>
 <div class="signinform text-center">
 <h4>Already a user? <a href="{{route('login')}}" class="hover-a">Sign In</a></h4>
 </div>
