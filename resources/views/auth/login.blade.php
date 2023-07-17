61472c19f282e08a12b0999feb873e03b99fa333
KwasiEvans <evansow2002@gmail.com>
2023-07-17 10:27:23 -1200

    completed login functionality

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,28 +15,30 @@
 <h3>Sign In</h3>
 <h4>Please login to your account</h4>
 </div>
-<div class="form-login">
-<label>Email</label>
-<div class="form-addons">
-<input type="text" placeholder="Enter your email address">
-<img src="assets/img/icons/mail.svg" alt="img">
-</div>
-</div>
-<div class="form-login">
-<label>Password</label>
-<div class="pass-group">
-<input type="password" class="pass-input" placeholder="Enter your password">
-<span class="fas toggle-password fa-eye-slash"></span>
-</div>
-</div>
-<div class="form-login">
-<div class="alreadyuser">
-<h4><a href="forgetpassword.html" class="hover-a">Forgot Password?</a></h4>
-</div>
-</div>
-<div class="form-login">
-<a class="btn btn-login" href="index.html">Sign In</a>
-</div>
+<form method="POST" action="{{ route('login') }}"> @csrf
+    <div class="form-login">
+        <label>Email</label>
+        <div class="form-addons">
+        <input type="text" placeholder="Enter your email address" name="email">
+        <img src="assets/img/icons/mail.svg" alt="img">
+        </div>
+        </div>
+        <div class="form-login">
+        <label>Password</label>
+        <div class="pass-group">
+        <input type="password" class="pass-input" placeholder="Enter your password" name="password">
+        <span class="fas toggle-password fa-eye-slash"></span>
+        </div>
+        </div>
+        <div class="form-login">
+        <div class="alreadyuser">
+        <h4><a href="forgetpassword.html" class="hover-a">Forgot Password?</a></h4>
+        </div>
+        </div>
+        <div class="form-login">
+        <button type="submit" class="btn btn-login">Sign In</button>
+        </div>
+</form>
 <div class="signinform text-center">
 <h4>Don’t have an account? <a href="{{route('register')}}" class="hover-a">Sign Up</a></h4>
 </div>
